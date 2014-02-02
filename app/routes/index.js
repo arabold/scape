8b1f8057c8cddc89bb83a34eac05ad9c31d6773d
Andre Rabold <arabold@nero.com>
2014-02-02 07:37:54 -0800

    Replaced spaces with tabs

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@
  * Module dependencies.
  */
 var config = require('../../config/config'),
-	_ = require('underscore');
+_ = require('underscore');
 
 module.exports.autoroute = {
 	get : {
@@ -11,8 +11,8 @@ module.exports.autoroute = {
 };
 
 function render(req, res) {
-    res.render('index', {
-    	app : config.app
-    });
+	res.render('index', {
+		app : config.app
+	});
 };
 
