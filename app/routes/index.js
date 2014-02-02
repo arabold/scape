39508d29a9de862369044c0e582070e3d4a01a1a
Andre Rabold <arabold@nero.com>
2014-02-02 07:01:52 -0800

    Added auto-routing with example

---
+++ app/routes/index.js
@@ -0,0 +1,18 @@
+/**
+ * Module dependencies.
+ */
+var config = require('../../config/config'),
+	_ = require('underscore');
+
+module.exports.autoroute = {
+	get : {
+		'/' : render
+	}
+};
+
+function render(req, res) {
+    res.render('index', {
+    	app : config.app
+    });
+};
+
